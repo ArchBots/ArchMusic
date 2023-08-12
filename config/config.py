91bcd0969a9230b13b909e0fcfbb6c2bc65a1807
ArchBots <140147861+ArchBots@users.noreply.github.com>
2023-08-12 14:29:12 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -136,7 +136,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
 
-GITHUB_REPO = getenv("GITHUB_REPO", None)
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/ArchBots/ArchMusic")
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 
