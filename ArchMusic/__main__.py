6c6280deb3a90e0c12522b88e728596c8de7b3ad
Chiranjib Koch <79559271+ChiranjibKoch@users.noreply.github.com>
2026-03-07 01:43:39 +0530

    idk

--- ArchMusic/__main__.py
+++ ArchMusic/__main__.py
@@ -75,8 +75,12 @@ async def init():
 
 
 if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     try:
-        asyncio.run(init())
+        loop.run_until_complete(init())
     except KeyboardInterrupt:
         pass
+    finally:
+        loop.close()
     LOGGER("ArchMusic").info("Stopping Arch Music Bot! GoodBye")
